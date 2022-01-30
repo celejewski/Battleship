6d02f70166207fbed128023f8223b8e633d1280a
celejewski <chrabaszc1988@tlen.pl>
2022-01-30 18:45:15 +0100

    Remove setters from Position

--- Battleship.Core/Models/Position.cs
+++ Battleship.Core/Models/Position.cs
@@ -9,9 +9,9 @@ namespace Battleship.Core.Models
     {
         private static readonly Random _random = new();
 
-        public int X { get; set; }
+        public int X { get; }
 
-        public int Y { get; set; }
+        public int Y { get; }
 
         public Position(int x, int y)
         {
